ec9a666ca28670a33017da6fb05836c68eaf8a58
mcreekmore <matthewacreekmore@gmail.com>
2024-02-05 22:01:41 -0500

    Brew formula update for godlp version v0.1.1

--- mcreekmore.rb
+++ mcreekmore.rb
@@ -5,11 +5,11 @@
 class Mcreekmore < Formula
   desc ""
   homepage "https://github.com/mcreekmore/godlp"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
-    url "https://github.com/mcreekmore/godlp/releases/download/v0.1.0/godlp_0.1.0_darwin_all.tar.gz"
-    sha256 "34ef177f5befb221b3c5e79f8d6382f725f45fe85448f8d40c4acadfc473ce13"
+    url "https://github.com/mcreekmore/godlp/releases/download/v0.1.1/godlp_0.1.1_darwin_all.tar.gz"
+    sha256 "0d67a0afbc09c034290af6248a7f7648a2b81df653a78dd5d8867442100a02ab"
 
     def install
       bin.install "godlp"
@@ -18,16 +18,16 @@ class Mcreekmore < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/mcreekmore/godlp/releases/download/v0.1.0/godlp_0.1.0_linux_amd64.tar.gz"
-      sha256 "34ba5e11d3bfd7737bab12041155683e1f0086ab72d29b55e7892a606ebd8560"
+      url "https://github.com/mcreekmore/godlp/releases/download/v0.1.1/godlp_0.1.1_linux_amd64.tar.gz"
+      sha256 "cfa81fd83b389636b0efd770e141f8c6fd76d5c9469388f040e6583c8430c19b"
 
       def install
         bin.install "godlp"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mcreekmore/godlp/releases/download/v0.1.0/godlp_0.1.0_linux_arm64.tar.gz"
-      sha256 "c53f17976a66d833b1ff163c550b6995827cb07f179e37919721bf818fb9bca3"
+      url "https://github.com/mcreekmore/godlp/releases/download/v0.1.1/godlp_0.1.1_linux_arm64.tar.gz"
+      sha256 "175fbab2709beff242b5ea5b4589dfb9d8516b6705dc28e5cc35397d3a4b7262"
 
       def install
         bin.install "godlp"
